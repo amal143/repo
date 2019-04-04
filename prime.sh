3dde0e1eb00ba10d8a98404bd8e3465cd2bbd87f
amal143 <amalbenny813@gmail.com>
2019-04-04 19:30:21 +0530

    first commit

---
+++ prime.sh
@@ -0,0 +1,16 @@
+echo "enter the numnber"
+read a
+i=2
+s=0
+while [ $i -lt $a ]
+do
+c=`expr $a % $i`
+if [ $c -eq $s ]
+then
+echo "not prime"
+exit
+else
+i=`expr $i + 1`
+if 
+done
+echo "prime number"
